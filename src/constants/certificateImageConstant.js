79d862861b89bae11b3b6ce99eefbac948dbe0d2
Manan Shah <manan228@yahoo.in>
2024-08-29 12:42:29 -0700

    code refactoring and adding of more certificate like python specialization, IOT specialization, postman

--- src/constants/certificateImageConstant.js
+++ src/constants/certificateImageConstant.js
@@ -1,9 +1,24 @@
-import Web_Design_Specialization_PNG from "../assets/Web_Design_Specialization.png";
-import Introduction_to_HTML5_PNG from "../assets/1_Introduction_to HTML5.png";
-import Introduction_to_CSS_PNG from "../assets/2_Introduction_to_CSS.png";
-import Interactivity_with_javaScript_PNG from "../assets/3_Interactivity_with_javaScript.png";
-import Advance_Styling_with_Responsive_Design_PNG from "../assets/4_Advance_Styling_with_Responsive_Design.png";
-import Capstone_PNG from "../assets/5_Capstone.png";
+import Web_Design_Specialization_PNG from "../assets/Web_Dev_Specialization/Web_Design_Specialization.png";
+import Introduction_to_HTML5_PNG from "../assets/Web_Dev_Specialization/1_Introduction_to HTML5.png";
+import Introduction_to_CSS_PNG from "../assets/Web_Dev_Specialization/2_Introduction_to_CSS.png";
+import Interactivity_with_javaScript_PNG from "../assets/Web_Dev_Specialization/3_Interactivity_with_javaScript.png";
+import Advance_Styling_with_Responsive_Design_PNG from "../assets/Web_Dev_Specialization/4_Advance_Styling_with_Responsive_Design.png";
+import Capstone_PNG from "../assets/Web_Dev_Specialization/5_Capstone.png";
+
+import Programming_for_Everybody_PNG from "../assets/Python_Specialization/1_Programming_for_Everybody.png";
+import Python_Data_Structure_PNG from "../assets/Python_Specialization/2_Python_Data_Structure.png";
+import Using_Python_to_access_Webdata_PNG from "../assets/Python_Specialization/3_Using_Python_to_access_Webdata.png";
+import Using_Database_with_Python_PNG from "../assets/Python_Specialization/4_Using_Database_with_Python.png";
+import Capstone_Python_PNG from "../assets/Python_Specialization/5_Capstone_Python.png";
+import Python_Specialization_PNG from "../assets/Python_Specialization/Python_Specialization.png";
+
+import Postman_Guided_Project_PNG from "../assets/Postman_Guided_Project.png";
+
+import Introduction_to_IOT_and_Embedded_System_PNG from "../assets/IOT/1_Introduction_to_IOT_and_Embedded_System.png";
+import The_Arduino_Platform_and_C_Programming_PNG from "../assets/IOT/2_The_Arduino_Platform_and_C_Programming.png";
+import Interfacing_with_Arduino_PNG from "../assets/IOT/3_Interfacing_with_Arduino.png";
+import The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_PNG from "../assets/IOT/4_The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi.png";
+import Interfacig_with_Rasbery_Pi_PNG from "../assets/IOT/5_Interfacig_with_Rasbery_Pi.png";
 
 export const Web_Design_Specialization_Certificate_PNG =
   Web_Design_Specialization_PNG;
@@ -14,3 +29,27 @@ export const Interactivity_with_javaScript_CERTIFICATE_PNG =
 export const Advance_Styling_with_Responsive_Design_CERTIFICATE_PNG =
   Advance_Styling_with_Responsive_Design_PNG;
 export const Capstone_CERTIFICATE_PNG = Capstone_PNG;
+
+export const Programming_for_EveryPDFbody_Certificate_PNG =
+  Programming_for_Everybody_PNG;
+export const Python_Data_Structure_Certificate_PNG = Python_Data_Structure_PNG;
+export const Using_Python_to_access_Webdata_Certificate_PNG =
+  Using_Python_to_access_Webdata_PNG;
+export const Using_Database_with_Python_Certificate_PNG =
+  Using_Database_with_Python_PNG;
+export const Capstone_Python_Certificate_PNG = Capstone_Python_PNG;
+export const Python_Specialization_Certificate_PNG = Python_Specialization_PNG;
+
+export const Postman_Guided_Project_CERTIFICATE_PNG =
+  Postman_Guided_Project_PNG;
+
+export const Introduction_to_IOT_and_Embedded_System_CERTIFICATE_PNG =
+  Introduction_to_IOT_and_Embedded_System_PNG;
+export const The_Arduino_Platform_and_C_Programming_CERTIFICATE_PNG =
+  The_Arduino_Platform_and_C_Programming_PNG;
+export const Interfacing_with_Arduino_CERTIFICATE_PNG =
+  Interfacing_with_Arduino_PNG;
+export const The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_CERTIFICATE_PNG =
+  The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_PNG;
+export const Interfacig_with_Rasbery_Pi_CERTIFICATE_PNG =
+  Interfacig_with_Rasbery_Pi_PNG;
--- src/constants/certificatePDFConstant.js
+++ src/constants/certificatePDFConstant.js
@@ -1,9 +1,24 @@
-import Web_Design_Specialization_PDF from "../assets/Web_Design_Specialization.pdf";
-import Introduction_to_HTML5_PDF from "../assets/1_Introduction_to HTML5.pdf";
-import Introduction_to_CSS_PDF from "../assets/2_Introduction_to_CSS.pdf";
-import Interactivity_with_javaScript_PDF from "../assets/3_Interactivity_with_javaScript.pdf";
-import Advance_Styling_with_Responsive_Design_PDF from "../assets/4_Advance_Styling_with_Responsive_Design.pdf";
-import Capstone_PDF from "../assets/5_Capstone.pdf";
+import Web_Design_Specialization_PDF from "../assets/Web_Dev_Specialization/Web_Design_Specialization.pdf";
+import Introduction_to_HTML5_PDF from "../assets/Web_Dev_Specialization/1_Introduction_to HTML5.pdf";
+import Introduction_to_CSS_PDF from "../assets/Web_Dev_Specialization/2_Introduction_to_CSS.pdf";
+import Interactivity_with_javaScript_PDF from "../assets/Web_Dev_Specialization/3_Interactivity_with_javaScript.pdf";
+import Advance_Styling_with_Responsive_Design_PDF from "../assets/Web_Dev_Specialization/4_Advance_Styling_with_Responsive_Design.pdf";
+import Capstone_PDF from "../assets/Web_Dev_Specialization/5_Capstone.pdf";
+
+import Programming_for_Everybody_PDF from "../assets/Python_Specialization/1_Programming_for_Everybody.pdf";
+import Python_Data_Structure_PDF from "../assets/Python_Specialization/2_Python_Data_Structure.pdf";
+import Using_Python_to_access_Webdata_PDF from "../assets/Python_Specialization/3_Using_Python_to_access_Webdata.pdf";
+import Using_Database_with_Python_PDF from "../assets/Python_Specialization/4_Using_Database_with_Python.pdf";
+import Capstone_Python_PDF from "../assets/Python_Specialization/5_Capstone_Python.pdf";
+import Python_Specialization_PDF from "../assets/Python_Specialization/Python_Specialization.pdf";
+
+import Postman_Guided_Project_PDF from "../assets/Postman_Guided_Project.pdf";
+
+import Introduction_to_IOT_and_Embedded_System_PDF from "../assets/IOT/1_Introduction_to_IOT_and_Embedded_System.pdf";
+import The_Arduino_Platform_and_C_Programming_PDF from "../assets/IOT/2_The_Arduino_Platform_and_C_Programming.pdf";
+import Interfacing_with_Arduino_PDF from "../assets/IOT/3_Interfacing_with_Arduino.pdf";
+import The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_PDF from "../assets/IOT/4_The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi.pdf";
+import Interfacig_with_Rasbery_Pi_PDF from "../assets/IOT/5_Interfacig_with_Rasbery_Pi.pdf";
 
 export const Web_Design_Specialization_Certificate =
   Web_Design_Specialization_PDF;
@@ -14,3 +29,26 @@ export const Interactivity_with_javaScript_CERTIFICATE =
 export const Advance_Styling_with_Responsive_Design_CERTIFICATE =
   Advance_Styling_with_Responsive_Design_PDF;
 export const Capstone_CERTIFICATE = Capstone_PDF;
+
+export const Programming_for_Everybody_CERTIFICATE =
+  Programming_for_Everybody_PDF;
+export const Python_Data_Structure_CERTIFICATE = Python_Data_Structure_PDF;
+export const Using_Python_to_access_Webdata_CERTIFICATE =
+  Using_Python_to_access_Webdata_PDF;
+export const Using_Database_with_Python_CERTIFICATE =
+  Using_Database_with_Python_PDF;
+export const Capstone_Python_CERTIFICATE = Capstone_Python_PDF;
+export const Python_Specialization_CERTIFICATE = Python_Specialization_PDF;
+
+export const Postman_Guided_Project_CERTIFICATE = Postman_Guided_Project_PDF;
+
+export const Introduction_to_IOT_and_Embedded_System_CERTIFICATE =
+  Introduction_to_IOT_and_Embedded_System_PDF;
+export const The_Arduino_Platform_and_C_Programming_CERTIFICATE =
+  The_Arduino_Platform_and_C_Programming_PDF;
+export const Interfacing_with_Arduino_CERTIFICATE =
+  Interfacing_with_Arduino_PDF;
+export const The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_CERTIFICATE =
+  The_Rasberry_Pi_and_Python_Programming_for_Rasberry_Pi_PDF;
+export const Interfacig_with_Rasbery_Pi_CERTIFICATE =
+  Interfacig_with_Rasbery_Pi_PDF;
